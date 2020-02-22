dd2882c6f8263d2594515ef1800a81b114792e72
AKSHAYCHIDRE <akshaychidre0@gmail.com>
2020-02-22 19:56:44 +0530

    updated home html page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FirstComponent } from './pages/first/first.component';
+import { SecondComponent } from './pages/second/second.component';
+import { ThirdComponent } from './pages/third/third.component';
+import { FourthComponent } from './pages/fourth/fourth.component';
+import { FifthComponent } from './pages/fifth/fifth.component';
 
+const routes: Routes = [
+  {
+    path: 'first',
+    component: FirstComponent
+  },
+  {
+    path: 'second',
+    component: SecondComponent
+  },
+  {
+    path: 'third',
+    component: ThirdComponent
+  },
+  {
+    path: 'fourth',
+    component: FourthComponent
+  },
+  {
+    path: 'fifth',
+    component: FifthComponent
+  }
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { FirstComponent } from './pages/first/first.component';
+import { SecondComponent } from './pages/second/second.component';
+import { ThirdComponent } from './pages/third/third.component';
+import { FourthComponent } from './pages/fourth/fourth.component';
+import { FifthComponent } from './pages/fifth/fifth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    FirstComponent,
+    SecondComponent,
+    ThirdComponent,
+    FourthComponent,
+    FifthComponent
   ],
   imports: [
     BrowserModule,
